ff96a9116d6214748e8498894a181b66b5ec251a
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2019-09-24 07:43:48 +0330

    plan cards added to introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,16 +1,48 @@
 <template>
-    <div class="container " id="template"  >
+    <div class="container main-div" id="template">
         <balatarHeader/>
 
-        <div class="row main_photo" id="main"  >
+        <div class="row main_photo" id="main">
             <div class=" card main_card col-5 " id="main-card">
                 <img class="card-img-top" src="../assets/wings-logo.png" style="margin-top: 10px"/>
                 <div class="card-body" style="text-align: center">
-                    <h4 style="color: #173c77"> سایت استخدامی  خود را داشته باشید.</h4><br/>
+                    <h4 style="color: #173c77"> سایت استخدامی خود را داشته باشید.</h4><br/>
                     <p>بالاتر برای شرکت شما یک سایت استخدامی اختصاصی می سازد. در این سایت شما می توانید شرکت خود را به
                         لحاظ ارزش ها و محیط کاری بطور کامل معرفی کنید.</p>
                     <p>همچنین می توانید تمامی موقعیت های شغلی خود را در این سایت قرار داده و به وسیله ابزارهای مدیریت
                         رزومه که ما در اختیار شما قرار می دهیم فرمان استخدام شرکت خودتان را به دست داشته باشید. </p>
+                </div>
+            </div>
+        </div>
+        <div class=" plans ">
+            <div class="plan card">
+                <div class=" plan-name">
+                    <h2>
+                        برنزی
+                    </h2>
+                </div>
+                <div class="card-body plan-disc">
+
+                </div>
+            </div>
+
+            <div class="plan card">
+                <div class=" plan-name">
+                    <h2>
+                       طلایی
+                    </h2>                </div>
+                <div class="card-body plan-disc">
+
+                </div>
+            </div>
+            <div class="plan card">
+                <div class=" plan-name">
+                    <h2>
+                        نقره ای
+                    </h2>
+                </div>
+                <div class="card-body plan-disc">
+
                 </div>
             </div>
         </div>
@@ -53,6 +85,7 @@
 
 <script>
     import BalatarHeader from "./BalatarHeader";
+
     export default {
         components: {BalatarHeader},
         props: [
@@ -89,6 +122,6 @@
 
 <!--</style>-->
 <style lang="scss" scoped>
-  @import "../styles/introduction.scss";
+    @import "../styles/introduction.scss";
 </style>
 <!--<link rel="stylesheet" href="../styles/introduction.scss">-->
