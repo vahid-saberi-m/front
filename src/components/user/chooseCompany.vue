b684e06378cebad5ad7863ef8690534712059239
vahid-saberi-m <33976610+vahid-saberi-m@users.noreply.github.com>
2019-09-24 07:01:28 +0330

    choose company refactor

--- src/components/user/chooseCompany.vue
+++ src/components/user/chooseCompany.vue
@@ -2,65 +2,70 @@
     <div class="" style="text-align: right; direction: rtl">
 
         <BalatarHeader/>
-        <div class="card bg-warning" v-if="companyId">
-            <div class="card-body">{{name}} عزیز تقاضای افزوده شدن شما در انتظار تایید مسوول مربوطه می باشد.</div>
-        </div>
-        <div class="card  choose-company-container" v-if="!companyId" style=" border-radius: 10px ">
-            <p v-if="!set">{{name}} عزیز. آیا می خواهید یک سایت استخدامی جدید بسازید یا اینکه دوست دارید به جمع کاربران سایت
-                استخدامی شرکتی بپیوندید؟</p>
-            <div class=" choose-action" v-if="!set">
-                <div class="card explanation">
-                    <b>ساخت یک سایت استخدامی جدید</b>
-                    <img src="../../assets/images/buildPage.png" class="action-img" />
-                    <p>
-                        چنانچه شرکت یا سازمان شما نیاز به ایجاد یک سایت استخدامی دارد از این طریق می توانید یک سایت استخدامی بسازید.
-                    </p>
-                    <button class="btn btn-success success" @click="set=1 ; newCompany=1">سایت استخدامی جدید</button>
-                </div>
-                <div class="card explanation">
-                    <b>عضویت در سایت استخدامی ساخته شده </b>
-                    <img src="../../assets/images/joinPage.jpg" class="action-img"/>
-                    <p>
-                        چنانچه شرکت یا سازمان شما از مشتریان ما است و می خواهید بعنوان کاربر به سایت استخدامی سازمان خود اضافه گردید از این طریق اقدام فرمایید.
-                    </p>
-                    <button class="btn btn-success" @click="set=1 ; newCompany=0">پیوستن به سایت استخدامی</button>
-                </div>
+        <div class="whole-page">
+            <div class="card bg-warning" v-if="companyId">
+                <div class="card-body">{{name}} عزیز تقاضای افزوده شدن شما در انتظار تایید مسوول مربوطه می باشد.</div>
             </div>
-
-
-            <form v-if="!newCompany&&set" class="form-group" enctype="multipart/form-data">
-                <div class="card-header card" style=" border-radius: 10px ">
-                    <label for="company_id">
-                        <h5>لطفا اطلاعات زیر را وارد کنید:</h5>
-                    </label>
-                </div>
-                <div class="card">
-                    <div class="card-body ">
-                        <label for="company_id"><strong>کد شرکت خود را وارد کنید:</strong></label>
-                        <input type="number" required class="input-group col-2" id="company_id" v-model="company_id"
-                               @input="searchCompany"/>
-                        <span class="badge badge-info col-4"
-                              style="font-size: medium; margin-top: 10px; margin-right: 10px">{{companyName}}</span>
-                        <br>
-                        <label for="position">
-                            <strong> رده سازمانی:</strong>
-                        </label>
-                        <input type="text" required class="input-group col-2" id="position" v-model="position"/>
-                        <label for="image"><strong> عکس:</strong></label>
-                        <input type="file" class="form-control-file col-2" id="image" name="image" accept="image/*"
-                               required @change="onFileSelected"/>
-                        <button type="button" class="btn btn-primary" style="margin-top: 10px" @click="joinCompany">ثبت
-                            اطلاعات
+            <div class="card  choose-company-container" v-if="!companyId" style=" border-radius: 10px ">
+                <p v-if="!set">{{name}} عزیز. آیا می خواهید یک سایت استخدامی جدید بسازید یا اینکه دوست دارید به جمع
+                    کاربران سایت
+                    استخدامی شرکتی بپیوندید؟</p>
+                <div class=" choose-action" v-if="!set">
+                    <div class="card explanation">
+                        <b>ساخت یک سایت استخدامی جدید</b>
+                        <img src="../../assets/images/buildPage.png" class="action-img"/>
+                        <p>
+                            چنانچه شرکت یا سازمان شما نیاز به ایجاد یک سایت استخدامی دارد از این طریق می توانید یک سایت
+                            استخدامی بسازید.
+                        </p>
+                        <button class="btn btn-success success" @click="set=1 ; newCompany=1">سایت استخدامی جدید
                         </button>
                     </div>
+                    <div class="card explanation">
+                        <b>عضویت در سایت استخدامی ساخته شده </b>
+                        <img src="../../assets/images/joinPage.jpg" class="action-img"/>
+                        <p>
+                            چنانچه شرکت یا سازمان شما از مشتریان ما است و می خواهید بعنوان کاربر به سایت استخدامی سازمان
+                            خود اضافه گردید از این طریق اقدام فرمایید.
+                        </p>
+                        <button class="btn btn-success" @click="set=1 ; newCompany=0">پیوستن به سایت استخدامی</button>
+                    </div>
                 </div>
-            </form>
 
-            <div class="card-body">
-                <form v-if="newCompany&&set" enctype="multipart/form-data">
-                    <div class="row" v-if="step===1">
-                        <div class="col-md-6">
-                            <div class="card-body border ">
+
+                <form v-if="!newCompany&&set" class="form-group" enctype="multipart/form-data">
+                    <div class="card-header card" style=" border-radius: 10px ">
+                        <label for="company_id">
+                            <h5>لطفا اطلاعات زیر را وارد کنید:</h5>
+                        </label>
+                    </div>
+                    <div class="card">
+                        <div class="card-body ">
+                            <label for="company_id"><strong>کد شرکت خود را وارد کنید:</strong></label>
+                            <input type="number" required class="input-group col-2" id="company_id" v-model="company_id"
+                                   @input="searchCompany"/>
+                            <span class="badge badge-info col-4"
+                                  style="font-size: medium; margin-top: 10px; margin-right: 10px">{{companyName}}</span>
+                            <br>
+                            <label for="position">
+                                <strong> رده سازمانی:</strong>
+                            </label>
+                            <input type="text" required class="input-group col-2" id="position" v-model="position"/>
+                            <label for="image"><strong> عکس:</strong></label>
+                            <input type="file" class="form-control-file col-2" id="image" name="image" accept="image/*"
+                                   required @change="onFileSelected"/>
+                            <button type="button" class="btn btn-primary" style="margin-top: 10px" @click="joinCompany">
+                                ثبت
+                                اطلاعات
+                            </button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="card-body main-form">
+                    <form v-if="newCompany&&set" enctype="multipart/form-data" class="main-form">
+                        <div class="row" v-if="step===1">
+                            <div class="col-md-6 card-body border form-section">
                                 <div class="form-group">
                                     <label for="name">نام شرکت شما:</label>
                                     <input type="text" class="form-control" id="name" aria-describedby="emailHelp"
@@ -82,9 +87,7 @@
                                            placeholder="logo">
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card-body border">
+                            <div class="card-body border form-section">
                                 <div class="form-check">
                                     <label for="website">آدرس وبسایت</label>
                                     <input type="text" class="form-control" id="website" placeholder="balatar.net"
@@ -107,78 +110,69 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row" v-if="step===2">
-                        <div class="col-md-12 border">
-                            <div class="card-body row">
-                                <div class="form-check col-md-6">
-                                    <label for="slogan">شعار استخدامی شما:</label>
-                                    <input type="text" class="form-control" id="slogan"
-                                           placeholder="به مجموعه ما بپیوندید" v-model="slogan">
-                                </div>
-                                <div class="form-check col-md-6">
-                                    <label for="mainPhoto">عکس اصلی سایت</label>
-                                    <input type="file" accept="image/*" class="form-control" id="mainPhoto"
-                                           placeholder="به مجموعه ما بپیوندید" @change="onMainPhotoSelected">
-                                </div>
-                            </div>
-                            <div class="card">
-                                <div class="card-header">
-                                    <strong>پیام شما به کارجویان:</strong>
-                                </div>
-                                <div class="card-body">
-                                    <div class="form-check">
-                                        <label for="messageTitle">عنوان پیام:</label>
-                                        <input type="text" class="form-control" id="messageTitle"
-                                               placeholder="به مجموعه ما بپیوندید" v-model="message_title">
+                        <div class="row" v-if="step===2">
+                            <div class="col-md-12 ">
+                                <div class="card-body row">
+                                    <div class="form-check col-md-6">
+                                        <label for="slogan">شعار استخدامی شما:</label>
+                                        <input type="text" class="form-control" id="slogan"
+                                               placeholder="به مجموعه ما بپیوندید" v-model="slogan">
                                     </div>
-                                    <div class="form-check">
-                                        <label for="messageContent">متن پیام:</label>
-                                        <textarea class="form-control" id="messageContent"
-                                                  placeholder="به مجموعه ما بپیوندید"
-                                                  v-model="message_content"></textarea>
+                                    <div class="form-check col-md-6">
+                                        <label for="mainPhoto">عکس اصلی سایت</label>
+                                        <input type="file" accept="image/*" class="form-control" id="mainPhoto"
+                                               placeholder="به مجموعه ما بپیوندید" @change="onMainPhotoSelected">
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header">
+                                        <strong>پیام شما به کارجویان:</strong>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="form-check">
+                                            <label for="messageTitle">عنوان پیام:</label>
+                                            <input type="text" class="form-control" id="messageTitle"
+                                                   placeholder="به مجموعه ما بپیوندید" v-model="message_title">
+                                        </div>
+                                        <div class="form-check">
+                                            <label for="messageContent">متن پیام:</label>
+                                            <textarea class="form-control" id="messageContent"
+                                                      placeholder="به مجموعه ما بپیوندید"
+                                                      v-model="message_content"></textarea>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row card" v-if="step===3">
-                        <div class=" col-md-12 card-header">
-
-                        </div>
-                        <div class="col-md-12 card-body">
-                            <div class="form-check">
+                        <div class="row " v-if="step===3">
+                            <div class="col-md-12 step3  ">
                                 <label for="whyUs">چرا باید با ما همکاری کنید؟</label>
                                 <textarea class="form-control" id="whyUs"
                                           placeholder="محیط کاری شرکت ما..."
                                           v-model="why_us"></textarea>
-                            </div>
-                            <div class="form-check">
                                 <label for="aboutUs">درباره ما:</label>
                                 <textarea class="form-control" id="aboutUs"
                                           placeholder="شرکت ما.."
                                           v-model="about_us"></textarea>
-                            </div>
-                            <div class="form-check">
                                 <label for="recruitingSteps">مراحل متداول استخدام در شرکت شما:</label>
                                 <textarea class="form-control" id="recruitingSteps"
                                           placeholder="شرکت ما.."
                                           v-model="recruiting_steps"></textarea>
                             </div>
                         </div>
-                    </div>
-                    <div class=" choose-company-pagination border " >
-                        <button class="btn btn-success col-centered " @click.prevent="nextStep" v-if="step<3"> بعدی
-                        </button>
-                        <button class="btn btn-warning col-centered" @click.prevent="previous" v-if="step>1"> قبلی
-                        </button>
-                    </div>
-                    <button type="submit" class="btn btn-primary col-centered" v-if="step===3"
+                    </form>
+                </div>
+                <div class="  choose-company-pagination border " v-if="newCompany&&set">
+                    <button class="btn btn-success " @click.prevent="nextStep"> بعدی
+                    </button>
+                    <button class="btn btn-warning " @click.prevent="previous" > قبلی
+                    </button>
+                    <button type="submit" class="btn btn-primary "
                             @click.prevent="makeCompany">ساخت شرکت
                     </button>
-                </form>
-            </div>
+                </div>
 
+            </div>
         </div>
     </div>
 </template>
@@ -249,10 +243,11 @@
             },
 
             nextStep() {
-                console.log('next step ==>', this.step)
+                if (this.step<3) 
                 this.step = this.step + 1;
             },
             previous() {
+                if (this.step>1)
                 this.step = this.step - 1;
             },
 
