fd6e45dbee9473475a2f6bee1fff302e79ad5019
vahid <saberi1365@gmail.com>
2019-10-01 16:19:19 +0330

    introduction description

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,73 +1,131 @@
 <template>
-    <div class="container main-div" id="template">
-        <balatarHeader/>
+  <div class="container main-div" id="template">
+    <balatarHeader/>
 
-        <div class="row main_photo" id="main">
-            <div class=" card main_card col-5 " id="main-card">
-                <img class="card-img-top" src="../assets/wings-logo.png" style="margin-top: 10px"/>
-                <div class="card-body" style="text-align: center">
-                    <h4 style="color: #173c77"> سایت استخدامی خود را داشته باشید.</h4><br/>
-                    <p>بالاتر برای شرکت شما یک سایت استخدامی اختصاصی می سازد. در این سایت شما می توانید شرکت خود را به
-                        لحاظ ارزش ها و محیط کاری بطور کامل معرفی کنید.</p>
-                    <p>همچنین می توانید تمامی موقعیت های شغلی خود را در این سایت قرار داده و به وسیله ابزارهای مدیریت
-                        رزومه که ما در اختیار شما قرار می دهیم فرمان استخدام شرکت خودتان را به دست داشته باشید. </p>
-                </div>
-            </div>
+    <div class="row main_photo " id="main">
+      <div class=" card main-card intro-card " id="main-card">
+        <img class="card-img-top" src="../assets/wings-logo.png" style="margin-top: 10px"/>
+        <div class="card-body ">
+          <h4 style="color: #173c77"> سایت استخدامی خود را داشته باشید.</h4><br/>
+          <p>بالاتر برای شرکت شما یک سایت استخدامی اختصاصی می سازد. در این سایت شما می توانید شرکت خود را به
+            لحاظ ارزش ها و محیط کاری بطور کامل معرفی کنید.</p>
+          <p>همچنین می توانید تمامی موقعیت های شغلی خود را در این سایت قرار داده و به وسیله ابزارهای مدیریت
+            رزومه که ما در اختیار شما قرار می دهیم فرمان استخدام شرکت خودتان را به دست داشته باشید. </p>
         </div>
-        <div class=" plans ">
-            <div class="plan card">
-                <div class=" plan-name">
-                    <h2>
-                        برنزی
-                    </h2>
-                </div>
-                <div class="card-body plan-disc">
-                  <p>2 آگهی شغلی همزمان</p>
-                  <p>دریافت روزانه حد اکثر 5 رزومه</p>
-                  <p>قابلیت تعریف حداکثر 1 سوال برای هر آگهی شغلی</p>
-                  <p>حد اکثر 14 روز نمایش هر آگهی</p>
-                  <p>گزارش حداکثر 2 رویداد دخلی </p>
-                  <p>بارگذاری حداکثر 5 عکس از محیط شرکت </p>
-                </div>
-              <button class="btn hover plan-buy">بیشتر...</button>
-
-            </div>
-
-            <div class="plan card " id="recommended">
-              <div class="annotation" >recommended</div>
-              <div class=" plan-name ">
-                    <h2>
-                       طلایی
-                    </h2>                </div>
-                <div class="card-body plan-disc">
-                  <p> بدون محدودیت در آگهی شغلی همزمان </p>
-                  <p>بدون محدودیت در دریافت رزومه</p>
-                  <p>بدون محدودیت در تعریف سوال برای هر آگهی شغلی</p>
-                  <p>بدون محدودیت در تعیین زمان انقضاء آگهی</p>
-                  <p>گزارش تمامی رویدادهای داخلی </p>
-                  <p>بارگذاری نامحدود عکس از محیط شرکت </p>
-                </div>
-              <button class="btn hover plan-buy">بیشتر...</button>
-
-            </div>
-            <div class="plan card">
-                <div class=" plan-name">
-                    <h2>
-                        نقره ای
-                    </h2>
-                </div>
-                <div class="card-body plan-disc">
-                  <p>5 آگهی شغلی همزمان</p>
-                  <p>دریافت روزانه حد اکثر 30 رزومه</p>
-                  <p>قابلیت تعریف حداکثر 3 سوال برای هر آگهی شغلی</p>
-                  <p>حد اکثر 30 روز نمایش هر آگهی</p>
-                  <p>گزارش حداکثر 5 رویداد دخلی </p>
-                  <p>بارگذاری حداکثر 50 عکس از محیط شرکت </p>
-                </div>
-              <button class="btn hover plan-buy">بیشتر...</button>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class=" plans ">
+      <div class="plan card">
+        <div class=" plan-name">
+          <h2>
+            برنزی
+          </h2>
+        </div>
+        <div class="card-body plan-disc">
+          <p>2 آگهی شغلی همزمان</p>
+          <p>دریافت روزانه حد اکثر 5 رزومه</p>
+          <p>قابلیت تعریف حداکثر 1 سوال برای هر آگهی شغلی</p>
+          <p>حد اکثر 14 روز نمایش هر آگهی</p>
+          <p>گزارش حداکثر 2 رویداد دخلی </p>
+          <p>بارگذاری حداکثر 5 عکس از محیط شرکت </p>
+        </div>
+        <button class="btn hover plan-buy">بیشتر...</button>
+
+      </div>
+
+      <div class="plan card " id="recommended">
+        <div class="annotation">recommended</div>
+        <div class=" plan-name ">
+          <h2>
+            طلایی
+          </h2></div>
+        <div class="card-body plan-disc">
+          <p> بدون محدودیت در آگهی شغلی همزمان </p>
+          <p>بدون محدودیت در دریافت رزومه</p>
+          <p>بدون محدودیت در تعریف سوال برای هر آگهی شغلی</p>
+          <p>بدون محدودیت در تعیین زمان انقضاء آگهی</p>
+          <p>گزارش تمامی رویدادهای داخلی </p>
+          <p>بارگذاری نامحدود عکس از محیط شرکت </p>
+        </div>
+        <button class="btn hover plan-buy">بیشتر...</button>
+
+      </div>
+      <div class="plan card">
+        <div class=" plan-name">
+          <h2>
+            نقره ای
+          </h2>
+        </div>
+        <div class="card-body plan-disc">
+          <p>5 آگهی شغلی همزمان</p>
+          <p>دریافت روزانه حد اکثر 30 رزومه</p>
+          <p>قابلیت تعریف حداکثر 3 سوال برای هر آگهی شغلی</p>
+          <p>حد اکثر 30 روز نمایش هر آگهی</p>
+          <p>گزارش حداکثر 5 رویداد دخلی </p>
+          <p>بارگذاری حداکثر 50 عکس از محیط شرکت </p>
+        </div>
+        <button class="btn hover plan-buy">بیشتر...</button>
+      </div>
+    </div>
+    <div class=" description">
+      <div class="recruiting-website">
+        <img class="recruiting-website-image" src="../assets/images/introduction/recruiting-website.jpg"/>
+
+        <div class="recruiting-website-description">
+          <div class="card-header title">
+            برند کارفمایی خود را تقویت کنید
+          </div>
+          <p> نیروی کار خوب در انتخاب شرکت آسان گیر
+            نیست. به همین خاطر است که شما در شرکتتان هزینه های متعددی می کنید تا نیروی خوبتان را از دست ندهید و در جذب
+            نیروی جدید انگیزه بیشتری ایجاد کنید. سوال اینجاست: نیروی کاری که شرکت شما را از نزدیک ندیده از کجا باید با
+            فضایی که خلق کردید آشنا شود؟
+            <b>سایت استخدامی جایی است که شما در آن ارزش ها، شرایط کاری و محیط شرکت خود را به کارجویان معرفی می کنید و از
+              آنان دعوت می کنید برای فرصت های شغلی آزادی که دارید اعلام آمادگی کنند.
+            </b>
+          </p>
+        </div>
+      </div>
+      <div class="recruiting-website">
+
+        <div class="recruiting-website-description">
+          <div class="card-header title">
+            هیچ رزومه ای را گم نکنید!
+          </div>
+          <p> از روزی که شرکت تاسیس شده تا بحال چند رزومه به دستتان رسیده؟ چندتا از این رزومه ها را بررسی کردید؟
+            چندتایشان را در گوشه ای گذاشته تا بعدها برای کار دیگری بررسی کنید؟ اگر این کارها را نکرده باشید منبع خوبی را
+            از دست دادید. اما اگر بخواهید این کار را انجام بدهید بدون ابزار کار سختی در پیش خواهید داشت.
+            <b>
+              بالاتر یک ابزار
+              مدیریت رزومه قوی در اختیار شما قرار می دهد تا هیچ رزومه ای را فراموش نکنید.
+            </b>
+          </p>
+        </div>
+        <img class="recruiting-website-image" src="../assets/images/introduction/workable.png"/>
+      </div>
+
+
+      <div class="recruiting-website">
+
+        <img class="recruiting-website-image" src="../assets/images/introduction/professional-recruitmemt.png"/>
+        <div class="recruiting-website-description">
+          <div class="card-header title">
+            حرفه ای استخدام کنید!
+          </div>
+          <p> رفتار حرفه ای در استخدام مراحل زیادی دارد، از ارسال ایمیل رسمی دعوت نامه گرفته تا اعلام رسمی پذیرفته نشدن
+            درخواست همکاری. کاری که متاسفانه بسیاری از شرکت ها به درست انجام نمی دهند! و این می تواند تاثیر منفی بزرگی
+            بر
+            برند کارفرمایی آن ها بگذارد.
+            <b>
+              با پنل استخدامی بالاتر می توانید تمامی مراحل استخدام و مصاحبه کاربر را با یک سیستم اتوماسیون مدیریت کنید
+              تا
+              حرفه ای ترین رفتار ممکن را در برابر کارجویان از خود نشان دهید.
+            </b>
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 </template>
 
@@ -106,6 +164,6 @@
 
 <!--</style>-->
 <style lang="scss" scoped>
-    @import "../styles/introduction.scss";
+  @import "../styles/introduction.scss";
 </style>
 <!--<link rel="stylesheet" href="../styles/introduction.scss">-->
