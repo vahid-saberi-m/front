a70481396a3fd9bc9d3611b53a9b08047054d46e
vahid <saberi1365@gmail.com>
2019-10-22 15:06:49 +0330

    client secret updated

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
     BASE_URL:'"http://api.bala-tar.ir"',
-    CLIENT_SECRET:'"umAv3VxpYspaUPdZzZhzQ36M74ljuKmKaouBuU0w"',
-    CLIENT_ID:'2'
+    CLIENT_SECRET:'"Yzl9uAzwt31VDZEfVXkZ2Q0GRKp8NaJkoMv46hG6"',
+    CLIENT_ID:'3'
 };
